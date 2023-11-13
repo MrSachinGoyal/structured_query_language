36e2c5dcc6f105b90ca3490f6179f07125d7f1a9
Sachin Goyal <sachingoel724@gmail.com>
2023-11-13 12:23:48 +0530

    Added SQL solution for finding node type of binary tree

--- sql_interview_questions.sql
+++ sql_interview_questions.sql
@@ -1,7 +1,6 @@
-CREATE DATABASE practice_till_placed;
+
 
 # DAY 1
-
 -- Question 1 Link: https://platform.stratascratch.com/coding/9726-classify-business-type?code_type=3
 -- Solution: 
 SELECT DISTINCT(business_name),
@@ -11,3 +10,14 @@ SELECT DISTINCT(business_name),
 		    ELSE 'other'
       END AS business_type
 FROM sf_restaurant_health_violations;
+
+# DAY 2
+-- Question 2 Link: https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
+-- Solution:
+SELECT N,
+       CASE WHEN P IS NULL THEN 'Root'
+            WHEN N IN (SELECT DISTINCT(P) FROM BST WHERE P IS NOT NULL) THEN 'Inner'
+            ELSE 'Leaf'
+        END AS Type
+FROM BST
+ORDER BY N;
