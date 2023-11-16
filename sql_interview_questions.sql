62da675d260be432a4ecdc631a7231067bd0e090
Sachin Goyal <sachingoel724@gmail.com>
2023-11-16 10:25:04 +0530

    Add SQL query to retrieve company information

--- sql_interview_questions.sql
+++ sql_interview_questions.sql
@@ -2,6 +2,7 @@
 
 # DAY 1
 -- Question 1 Link: https://platform.stratascratch.com/coding/9726-classify-business-type?code_type=3
+
 -- Solution: 
 SELECT DISTINCT(business_name),
 	   CASE WHEN business_name LIKE '%restaurant%' THEN 'restaurant'
@@ -13,6 +14,7 @@ FROM sf_restaurant_health_violations;
 
 # DAY 2
 -- Question 2 Link: https://www.hackerrank.com/challenges/binary-search-tree-1/problem?isFullScreen=true
+
 -- Solution:
 SELECT N,
        CASE WHEN P IS NULL THEN 'Root'
@@ -21,3 +23,25 @@ SELECT N,
         END AS Type
 FROM BST
 ORDER BY N;
+
+# DAY 3
+-- Question 3 Link: https://www.hackerrank.com/challenges/the-company/problem?isFullScreen=true
+
+-- Solution:
+SELECT c.company_code, 
+       c.founder AS founder_name,
+       COUNT(DISTINCT(lm.lead_manager_code)) AS lead_manager_count,
+       COUNT(DISTINCT(sm.senior_manager_code)) AS senior_manager_count,
+       COUNT(DISTINCT(mgr.manager_code)) AS manager_count,
+       COUNT(DISTINCT(emp.employee_code)) AS emp_count
+FROM company c
+INNER JOIN lead_manager lm
+ON c.company_code = lm.company_code
+LEFT JOIN senior_manager sm
+ON lm.lead_manager_code = sm.lead_manager_code
+LEFT JOIN manager mgr
+ON sm.senior_manager_code = mgr.senior_manager_code
+LEFT JOIN employee emp
+ON mgr.manager_code = emp.manager_code
+GROUP BY c.company_code, c.founder
+ORDER BY c.company_code;
